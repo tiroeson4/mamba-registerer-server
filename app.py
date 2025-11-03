549f75607c27a2fc9be3bd1e648f28da6a0ab7b6
tiroeson4 <160613353+tiroeson4@users.noreply.github.com>
2025-11-03 21:05:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def get_config():
 
     return jsonify({
         "default": default_pid,
-        "profiles": conf.get("profiles", {})
+        "profiles": conf.get("profiles", {}),
         "positions": conf.get("positions", {})
     })
 
@@ -148,4 +148,5 @@ def index():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 3000))
     app.run(host="0.0.0.0", port=port)
+
 
