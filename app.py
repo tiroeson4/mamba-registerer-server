b785a5cf7ea348c08dccb5e717a39792d4940fbb
tiroeson4 <160613353+tiroeson4@users.noreply.github.com>
2025-11-11 15:16:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -148,11 +148,6 @@ def set_cities():
 def index():
     return "✅ Mamba Registerer server is running!"
 
-@app.route("/")
-def index():
-    return "✅ Mamba Registerer server is running!"
-
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 3000))
     app.run(host="0.0.0.0", port=port)
